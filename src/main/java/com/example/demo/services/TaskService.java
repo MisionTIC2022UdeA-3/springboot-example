41645c458082a5eb9ee77d4f73c952632d1dbdda
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-07-22 13:00:12 +0100

    agregar servicios y controladores

---
+++ src/main/java/com/example/demo/services/TaskService.java
@@ -0,0 +1,25 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.Task;
+import com.example.demo.entities.TaskList;
+
+import java.time.LocalDate;
+
+public class TaskService {
+    Task tarea1;
+    Task tarea2;
+    TaskList propositos;
+
+    public TaskService(){
+        this.tarea1 = new Task("Aprender Java", false, LocalDate.of(2022,12,31) );
+        this.tarea2 = new Task("Aprender HTML", false, LocalDate.of(2022,12,31) );
+        this.propositos = new TaskList("Propositos fin de año");
+        propositos.addTask(tarea1);
+        propositos.addTask(tarea2);
+    }
+
+    public TaskList getTaskList(){
+        return this.propositos;
+    }
+
+}
