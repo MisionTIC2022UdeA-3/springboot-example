fa439739635c5e7e00cad23be5ed6ae02f59fe00
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2022-07-31 10:41:09 +0100

    finish authentication example

--- src/main/java/com/example/demo/controllers/FrontControllers.java
+++ src/main/java/com/example/demo/controllers/FrontControllers.java
@@ -1,7 +1,11 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entities.Task;
+import com.example.demo.entities.User;
 import com.example.demo.services.TaskService;
+import com.example.demo.services.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +16,18 @@ import java.util.List;
 public class FrontControllers {
 
     TaskService service;
-    public FrontControllers(TaskService service){
+    UserService userService;
+    public FrontControllers(TaskService service, UserService userService){
         this.service = service;
+        this.userService = userService;
     }
 
     @GetMapping("/")
-    public String index(){
+    public String index (Model model, @AuthenticationPrincipal OidcUser principal){
+        if(principal != null){
+            User user = this.userService.getOrCreateUser( principal.getClaims());
+            model.addAttribute("user", user);
+        }
         return "index";
     }
 
